7f660f4480eec90f4b61348d05dd37fccb657573
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2024-01-02 21:51:34 -0300

    added disko.nix draft for testing in vm

---
+++ nixos/ultra-disko.nix
@@ -0,0 +1,65 @@
+{ ... }: {
+  disko.devices = {
+    disk = {
+      sda = { # name of attr set can be anything, name for drive for convenience 
+        type = "disk";
+        device = "/dev/sda"; # can also be declared from the "disks" attr with "builtins.elemAt disks 0;"
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = {
+            ESP = {
+              name = "ESP";
+              start = "1MiB";
+              end = "512MiB";
+              fs-type = "fat32";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            luks = {
+              name = "ultra_crypted";
+              type = "luks";
+              start = "512MiB";
+              end = "100%";
+              passwordFile = "/tmp/secret.key"; # interactive password entry
+              settings.allowDiscards = true;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                postCreateHook = ''
+                  btrfs subvolume snapshot -r /mnt/@root /mnt/@snapshots/@root-blank
+                ''
+                subvolumes = {
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "4G";
+                  };
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@snapshots" = {
+                    mountpoint = "/.snapshots";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+	          };
+            };
+          };
+        };
+      };
+    };
+  };
+}
