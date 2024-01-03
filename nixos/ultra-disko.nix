ad097e0654ac6f0f48ecc2e9019125c385c10cb5
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2024-01-03 01:08:53 -0300

    first error free version of disko config

--- nixos/ultra-disko.nix
+++ nixos/ultra-disko.nix
@@ -5,59 +5,60 @@
         type = "disk";
         device = "/dev/sda"; # can also be declared from the "disks" attr with "builtins.elemAt disks 0;"
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
+          type = "gpt";
+          partitions = {
+            ESP = {
               name = "ESP";
               start = "1MiB";
               end = "512MiB";
-              fs-type = "fat32";
-              bootable = true;
+              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-              };
-            }
-            {
-              name = "ultra_crypted";
-              type = "luks";
+                mountOptions = [ "defaults" ];
+			  };
+            };
+            luks = {
               start = "512MiB";
               end = "100%";
-              passwordFile = "/tmp/secret.key"; # interactive password entry
-              settings.allowDiscards = true;
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
-                postCreateHook = ''
-                  btrfs subvolume snapshot -r /mnt/@root /mnt/@snapshots/@root-blank
-                '';
-                subvolumes = {
-                  "@persist" = {
-                    mountpoint = "/persist";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                name = "ultra_crypted";
+                type = "luks";
+			    askPassword = true;
+				settings.allowDiscards = true;
+				content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ]; # Override existing partition
+                  postCreateHook = ''
+                    btrfs subvolume snapshot -r /mnt/@root /mnt/@snapshots/@root-blank
+                  '';
+                  subvolumes = {
+                    "@persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "@swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "4G";
+                    };
+                    "@root" = {
+                      mountpoint = "/";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "@snapshots" = {
+					  mountpoint = "/.snapshots";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "@home" = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+				    };
                   };
-                  "@swap" = {
-                    mountpoint = "/.swapvol";
-                    swap.swapfile.size = "4G";
-                  };
-                  "@root" = {
-                    mountpoint = "/";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "@snapshots" = {
-                    mountpoint = "/.snapshots";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "@home" = {
-                    mountpoint = "/home";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                };
-	          };
-            }
-          ];
+	            };
+              };
+            };
+          };
         };
       };
     };
