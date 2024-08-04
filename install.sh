e81fb1462b23bafb9f1bcc40b9f88a5356b31362
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2024-08-04 00:55:44 -0300

    improved install script

--- install.sh
+++ install.sh
@@ -4,30 +4,47 @@ configs=(
   ultra
 )
 
+NO_INSTALL=1
+
+if [[ $# -eq 0 ]]; then
+  echo "Provide a Nix Flake config name"
+  exit
+fi
+
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
       echo "Provide a Nix Flake config name"
-      break
+      exit
       ;;
     -f|--flake)
       FLAKE=$2
       shift # past argument
       shift # past value
       ;;
+  --format-only)
+      NO_INSTALL=0
+      shift
+      ;;
     *)
       echo "Error: no known argument provided"
-      break
+      exit 1
       ;;
   esac
 done
 
 for config in $configs; do
     if [[ "$config" == "$FLAKE" ]]; then
-      nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko --flake ".#${config}"
-      break
+      if [[ NO_INSTALL -eq 0 ]]; then
+        nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko --flake ".#${config}"
+        exit
+      else
+        nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko --flake ".#${config}"
+        nixos-install --root /mnt --flake ".#${config}"
+        exit
+      fi
     else
       echo "Error: unknown configuration provided"
-      break
+      exit 1
     fi
 done
