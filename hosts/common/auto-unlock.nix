3591628cfaaa0801b10480c8ab7bd01a3707d2d5
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2024-09-08 14:10:13 -0300

    added sddm autologin

--- hosts/common/auto-unlock.nix
+++ hosts/common/auto-unlock.nix
@@ -1,4 +1,5 @@
-{ inputs
+{ lib
+, globals
 , pkgs
 }:let
     script = pkgs.writeShellScriptBin "unlocker" ''
@@ -9,7 +10,41 @@
     fi
   ''
   in {
-  security.pam.services.keepassxc.text = ''
+  security.pam.services = {
+    keepassxc.text = ''
     session optional pam_exec.so expose_authtok  ${script}
-  ''
+  '';
+    sddm-autologin.text = ''
+      auth     requisite pam_nologin.so
+	  auth     required  pam_succeed_if.so uid >= ${lib.toString config.services.displayManager.sddm.autoLogin.minimumUid} quiet
+	  auth     required  pam_permit.so
+	  auth     optional  pam_systemd_loadkey.so
+				
+	  account  include   sddm
+			
+	  password include   sddm
+				
+	  session  include   sddm
+    '';
+  };
+
+  systemd.services.display-manager = {
+    after = [
+      "systemd-user-sessions.service"
+      "getty@tty7.service"
+      "plymouth-quit.service"
+      "systemd-logind.service"
+    ];
+    conflicts = [
+      "getty@tty7.service"
+    ];
+    serviceConfig = { KeyringMode = "inherit"; };
+  };
+
+  services.displayManager.sddm.settings = {
+    Autologin = {
+      Session = "plasma";
+      User = "${globals.ultra.userName}";
+    };
+  };
 }
