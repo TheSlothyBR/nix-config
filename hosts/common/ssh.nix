a7a8c8e9c3964d58dac63c0c61f2711b6a8d3644
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2024-09-25 21:33:10 -0300

    switched ssd keys to non declarative

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -16,19 +16,19 @@ in {
       AcceptEnv = "WAYLAND_DISPLAY";
       X11Forwarding = true;
     };
-    hostKeys = [
-      {
-        path = "/persist/system/etc/ssh/${globals.ultra.hostName}_ed25519_key";
-        type = "ed25519";
-      }
-    ];
+    #hostKeys = [
+    #  {
+    #    path = "/persist/system/etc/ssh/${globals.ultra.hostName}_ed25519_key";
+    #    type = "ed25519";
+    #  }
+    #];
   };
   
-  programs.ssh = {
+  #programs.ssh = {
     #knownHosts = lib.genAttrs hosts (hostname: {
     #  publicKeyFile = ./secrets/${hostname}_ed25519_key.pub;
     #});
-  };
+  #};
 
   security.pam.sshAgentAuth = {
     enable = true;
