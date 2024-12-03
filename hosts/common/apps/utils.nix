3f7c1b70d962a177611c2d38ce11b553c285a89d
/run/secrets/git/name </run/secrets/git/email>
2024-12-03 18:22:19 -0300

    migrated to nh nix option

--- hosts/common/apps/utils.nix
+++ hosts/common/apps/utils.nix
@@ -10,18 +10,26 @@
   };
 
   config = lib.mkIf config.custom.utils.enable {
+    programs = {
+      nh = {
+        enable = true;
+        flake = "/etc/nixos/dotfiles";
+        #clean.extraArgs = "--keep-since 2d";
+      };
+    };
+
     environment = {
       systemPackages = with pkgs; [
         wget
         wl-clipboard
         tealdeer
-      ] ++ [
-        nh
-        nix-output-monitor
-        nvd
+      #] ++ [
+      #  nh
+      #  nix-output-monitor
+      #  nvd
       ];
       sessionVariables = {
-        FLAKE = "/etc/nixos/dotfiles";
+      #  FLAKE = "/etc/nixos/dotfiles";
       };
     };
   };
