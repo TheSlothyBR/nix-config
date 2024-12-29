ee8721fca017c9949cf3fa362235d5834db2bb6f
/run/secrets/git/name </run/secrets/git/email>
2024-12-29 18:57:37 -0300

    added quick commands to customIso

--- hosts/customIso/configuration.nix
+++ hosts/customIso/configuration.nix
@@ -27,5 +27,25 @@
     git
     curl
     fsarchiver
+    (pkgs.writeShellApplication {
+      name = "install";
+      runtimeInputs = [  ];
+      text = ''
+        nix run github:${globals.meta.owner}/${globals.meta.repo} -- "$@"
+      '';
+    })
+    (pkgs.writeShellApplication {
+      name = "wipe";
+      runtimeInputs = [  ];
+      text = ''
+        mount -o remount,size=2G,noatime /nix/.rw-store
+        swapoff /dev/zram0
+        modprobe -r zram
+        echo 1 > /sys/module/zswap/parameters/enabled
+        umount -A /tmp/usb
+        unset SOPS_AGE_KEY_FILE
+        rm -rf /{dotfiles,tmp/usb,tmp/*{.age,_key,_key.pub}}
+      '';
+    })
   ];
 }
