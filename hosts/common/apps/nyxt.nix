70d8ac87e2bc00b5bc4198d2dc7f6a28f4f3541b
TheSlothyBR <95154797+TheSlothyBR@users.noreply.github.com>
2025-01-10 18:46:12 -0300

    added nyxt browser flatpak

---
+++ hosts/common/apps/nyxt.nix
@@ -0,0 +1,33 @@
+{ config
+, isUser
+, lib
+, ...
+}:{
+  options = {
+    custom.nyxt = {
+      enable = lib.mkEnableOption "Nyxt Browser config";
+    };
+  };
+
+  config = lib.mkIf config.custom.nyxt.enable {
+    home-manager.users.${isUser} = {
+      services.flatpak = {
+        packages = [
+          {
+            appId = "engineer.atlas.Nyxt";
+            origin = "flathub";
+          }
+        ];
+      };
+    };
+
+    environment.persistence."/persist" = {
+      users.${isUser} = {
+        directories = [
+          ".var/app/engineer.atlas.Nyxt"
+        ];
+      };
+    };
+  };
+  };
+}
