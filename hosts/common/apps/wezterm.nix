deca83460c756400093d12d066228c3368d3e623
TheSlothyBR <caiodesigner.97@hotmail.com>
2025-01-21 02:26:04 -0300

    changed wezterm opacity and plasma panel settings

--- hosts/common/apps/wezterm.nix
+++ hosts/common/apps/wezterm.nix
@@ -1,9 +1,11 @@
-{ isUser
-, config
-, lib
-, pkgs
-, ...
-}:{
+{
+  isUser,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   options = {
     custom.wezterm = {
       enable = lib.mkEnableOption "Wezterm config";
@@ -15,7 +17,7 @@
       wezterm
     ];
 
-   systemd.services."generate-wezterm-config" = {
+    systemd.services."generate-wezterm-config" = {
       description = "Generate Wezterm Config";
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
@@ -24,30 +26,44 @@
         Group = "users";
       };
       script = ''
-        mkdir -p ~/.config/wezterm
-        cat << 'EOF' > ~/.config/wezterm/wezterm.lua
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
--- local colors = dofile(os.getenv("HOME") .. "/<path>/<to>/<name>.lua")
+                 mkdir -p ~/.config/wezterm
+                 cat << 'EOF' > ~/.config/wezterm/wezterm.lua
+        local wezterm = require("wezterm")
+        local config = wezterm.config_builder()
+        -- local colors = dofile(os.getenv("HOME") .. "/<path>/<to>/<name>.lua")
 
-config = {
-  front_end = "WebGpu",
-  enable_kitty_graphics = true,
-  max_fps = 60,
-  -- window_close_confirmation = "NeverPrompt",
-  -- font = wezterm.font("<nameHere>"),
-  window_padding = {
-    left = 2,
-    right = 2,
-    top = 15,
-    bottom = 0,
-  },
-  font_size = 11.0,
-  -- colors = {},
-}
+        config = {
+          enable_tab_bar = true,
+          front_end = "WebGpu",
+          enable_kitty_graphics = true,
+          max_fps = 60,
+          window_padding = {
+            left = 2,
+            right = 2,
+            top = 15,
+            bottom = 0,
+          },
+          font_size = 11.0,
+          window_background_opacity = 0.8,
+          use_fancy_tab_bar = false,
+          hide_tab_bar_if_only_one_tab = true,
+          colors = {
+            tab_bar = {
+              background = "rgba(0,0,0,0.8)",
+        --    active_tab = {
+        --      bg_color = "rgba(0,0,0,1)",
+        --      fg_color = "white",
+        --    },
+        --    new_tab = {
+        --      bg_color = "rgba(0,0,0,1)",
+        --      fg_color = "white",
+        --    },
+            };
+          },
+        }
 
-return config
-EOF
+        return config
+        EOF
       '';
     };
   };
